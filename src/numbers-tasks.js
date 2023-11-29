f0af80db496b7f7548d62c0e4c6249865a06049d
aoneban <torgregion1@gmail.com>
2023-11-29 20:40:17 +0100

    feat: finish numbers task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -270,8 +270,18 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) return 0;
+  const arr = [0, 1, 1];
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= index; i += 1) {
+    const c = a + b;
+    arr.push(c);
+    a = b;
+    b = c;
+  }
+  return arr[arr.length - 1];
 }
 
 /**
